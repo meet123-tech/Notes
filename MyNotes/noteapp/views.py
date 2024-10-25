3d3e408b6633c813727f50358a318953b7fb0672
meet123-tech <savaliyameet15@gmail.com>
2024-10-25 12:29:14 +0530

    create an api view using rest

--- MyNotes/noteapp/views.py
+++ MyNotes/noteapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from noteapp.serializer import NoteSerializer
+from .models import Note
 # Create your views here.
+
+
+@api_view(["GET","POST"])
+def notes(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = NoteSerializer(data = request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
