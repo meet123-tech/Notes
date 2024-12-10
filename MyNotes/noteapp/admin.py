25c09f2ed0ebc51b5c975e8a07a9c498e981ad41
meet123-tech <savaliyameet15@gmail.com>
2024-12-10 11:24:41 +0530

    resume

--- MyNotes/noteapp/admin.py
+++ MyNotes/noteapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Note
 # Register your models here.
-admin.site.register(Note)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "created", "updated"]
+
+
+admin.site.register(Note,NoteAdmin)
 
--- MyNotes/noteapp/models.py
+++ MyNotes/noteapp/models.py
@@ -12,7 +12,7 @@ class Note(models.Model):
     
     title = models.CharField(max_length=100)
     body = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     category = models.CharField(max_length=15, choices=CATEGORY, default='PERSONAL')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
